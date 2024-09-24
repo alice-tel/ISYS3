eab6662b86ae18efca110d67d1264f792b43aa5e
ccvis <cc.vis@st.hanze.nl>
2024-09-24 20:04:37 +0200

    Grid voor zeeslag

---
+++ zeeslagje/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) {
+        // Verander straks ZeeslagGRID naar het bestand van het werkende spel :)
+        ZeeslagGRID zeeslag = new ZeeslagGRID();
+    }
+}
---
+++ zeeslagje/ZeeslagGRID.java
@@ -0,0 +1,49 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class ZeeslagGRID {
+    int boardWidth = 800;
+    int boardHeight = 850;
+
+    JFrame frame = new JFrame("Zeeslag");
+    JLabel textLabel = new JLabel();
+    JPanel textPanel = new JPanel();
+    JPanel boardPanel = new JPanel();
+
+    JButton[][] board = new JButton[8][8];
+
+    ZeeslagGRID() {
+        frame.setVisible(true);
+        frame.setSize(boardWidth,boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+
+        textLabel.setBackground(Color.darkGray);
+        textLabel.setForeground(Color.white);
+        textLabel.setFont(new Font("Arial", Font.BOLD, 50));
+        textLabel.setHorizontalAlignment(JLabel.CENTER);
+        textLabel.setText("Zeeslag");
+        textLabel.setOpaque(true);
+
+        textPanel.setLayout(new BorderLayout());
+        textPanel.add(textLabel);
+        frame.add(textPanel,BorderLayout.NORTH);
+
+        boardPanel.setLayout(new GridLayout(8, 8));
+        boardPanel.setBackground(Color.darkGray);
+        frame.add(boardPanel);
+
+        for (int r = 0; r < 8; r++) {
+            for (int c = 0; c < 8; c++) {
+                JButton tile = new JButton();
+                tile.setBackground(Color.darkGray);
+                tile.setPreferredSize(new Dimension(100,100));
+                board[r][c] = tile;
+                boardPanel.add(tile);
+            }
+        }
+        frame.pack();
+    }
+}
