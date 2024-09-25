d1ee6ba19921f09ad26227e7a6e562338abbcaba
alice <a.tel@st.hanze.nl>
2024-09-25 09:16:11 +0200

    changed main app classes to their respective game names

--- tictactoe/TicTacToeApp.java
+++ tictactoe/TicTacToeApp.java
@@ -1,4 +1,4 @@
-public class App {
+public class TicTacToeApp {
     public static void main(String[] args) throws Exception {
         TicTacToe ticTacToe = new TicTacToe();
     }
--- zeeslagje/ZeeslagjeApp.java
+++ zeeslagje/ZeeslagjeApp.java
@@ -1,4 +1,4 @@
-public class App {
+public class ZeeslagjeApp {
     public static void main(String[] args) {
         // Verander straks ZeeslagGRID naar het bestand van het werkende spel :)
         ZeeslagGRID zeeslag = new ZeeslagGRID();
