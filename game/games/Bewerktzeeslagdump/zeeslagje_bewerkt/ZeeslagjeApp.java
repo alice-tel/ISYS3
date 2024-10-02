ecf003ef8d6c2ccc2d8e77dc5e5c868928a9b647
job14142 <163145054+job14142@users.noreply.github.com>
2024-10-02 11:25:55 +0200

    -launcher werkt weer voor job

--- game/games/Bewerktzeeslagdump/zeeslagje_bewerkt/ZeeslagjeApp.java
+++
@@ -1,9 +0,0 @@
-
-
-public class ZeeslagjeApp {
-    public static void main(String[] args) {
-        // Verander straks ZeeslagGRID naar het bestand van het werkende spel :)
-        ZeeSlag zeeslag = new ZeeSlag();
-//        ZeeslagLayoutPhase();
-    }
-}
--- game/launcher/GameLauncher.java
+++ game/launcher/GameLauncher.java
@@ -8,7 +8,6 @@ import javax.swing.*;
  */
 public class GameLauncher {
     public static void main(String[] args) {
-
         SwingUtilities.invokeLater(GameSelectionFrame::new);
     }
 }
