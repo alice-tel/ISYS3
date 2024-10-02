e93fce275ee8d02ae99c1dc73e7cf9426578a087
Tubsy <80598416+Tubsy@users.noreply.github.com>
2024-10-02 10:31:55 +0200

    Added option to choose Player vs Computer option for TicTacToe, still WIP.

--- game/launcher/GameOptionsFrame.java
+++ game/launcher/GameOptionsFrame.java
@@ -56,7 +56,7 @@ public class GameOptionsFrame extends JFrame {
         if (gameName.equals("Tic-Tac-Toe") && mode.equals("Player vs Player")) {
             new TicTacToeGame(); // Start PvP Tic Tac Toe game
         } else if (gameName.equals("Tic-Tac-Toe") && mode.equals("Player vs Computer")) {
-            new TicTacToeGame(); // Start Tic Tac Toe game, replace with PvC game when finished!!
+            new TicTacToeGame(); // Start Tic Tac Toe game, replace with PvC game when finished
         }
         System.out.println("Starting " + gameName + " in " + mode + " mode.");
     }
