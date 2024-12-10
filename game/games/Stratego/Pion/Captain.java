54da645be3d19d7ce1cff71d5b780d9ce16e3876
pietpuk <m.hingstman@st.hanze.nl>
2024-12-10 12:10:15 +0100

    Captain en colonel gedaan, iemand moet ff checken of dat zo goed is.

--- game/games/Stratego/Pion/Captain.java
+++ game/games/Stratego/Pion/Captain.java
@@ -1,5 +1,30 @@
 package game.games.Stratego.Pion;
 
-public class Captain {
+import game.games.Stratego.Strategys.MoveStrategy;
+import game.games.Stratego.Strategys.PionStrategy;
+
+
+public class Captain implements PionStrategy {
+    private MoveStrategy moveStrategy; // Field to store the strategy
+
+    @Override
+    public int getMoveDistance() {
+        return 1; // Scouts can move 9 spaces
+    }
+
+    @Override
+    public int getHealth() {
+        return 6; // Scouts have a health value of 9
+    }
+
+    @Override
+    public MoveStrategy getMoveStrategy() {
+        return moveStrategy; // Return the current strategy
+    }
+
+    @Override
+    public void setMoveStrategy(MoveStrategy moveStrategy) {
+        this.moveStrategy = moveStrategy; // Assign a new strategy
+    }
     
 }
